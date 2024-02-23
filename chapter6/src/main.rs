d2683b93b7692cafa60ff8fcad89909d1636d52b
DiegoZz <dfflores@espol.edu.ec>
2024-02-23 16:54:01 -0500

    DiegoZz

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+//ENUMS
+//Option<T>
+enum IpAddrKind{
+    v4,
+    v6,
 }
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+/*impl Message{
+    fn call(&self){
+        println!("{}", self.)
+    }
+}
+*/
+struct server{
+    IpAdrr: IpAddrKind,
+    some: u32,
+}
+fn print_addr(add: IpAddrKind){
+    
+}
+
+fn main() {
+   let four = IpAddrKind:: v4; 
+    let s: Option<String> = Some(String::from("XD"));
+    match s {
+        Some(value)=>{
+                println!("hihi");
+        }
+        None => {
+                println!("None");
+        }
+    }
+    let m = Message::Write(String::from("Diego"));
+    match m {
+        Message::Write(s) => {
+            println!("U were right");
+        }
+        Message::Quit => {
+            println!("QUIT");
+        }
+        Message::Move{x,y} =>{
+            println!("Move");
+        }
+        Message::ChangeColor(color1,color2,color3)=>{
+            println!("ChangeColor");
+        }
+    }
+}
+
+
+
